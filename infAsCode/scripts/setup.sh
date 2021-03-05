c3cf067f2c88c3a1d83a874073bb33607a75cedd
steinko <steinkor@gmail.com>
2021-03-05 16:20:56 +0100

    remove sudo

--- infAsCode/scripts/setup.sh
+++ infAsCode/scripts/setup.sh
@@ -10,7 +10,7 @@ export PATH=$PATH:$HOME/.pulumi/bin
 pulumi login
 
 # nodejs
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | bash
 apk add nodejs
 # yarn
 npm i -g yarn
